ff6dbe3dfd2eb27fc115c018df3f700207ffce2e
stanislawzambrzycki <stanislaw.zambrzycki@poczta.onet.pl>
2020-10-03 03:46:09 +0200

    fine styling, colours change, changed contact page into button

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,11 +3,16 @@ const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
+const contactBtn = document.querySelector(".contact-btn");
 
 const navItems = document.querySelectorAll(".nav-item");
+const contactItems = document.querySelectorAll(".contact-item");
 
 // Set Initial State of Menu
 let showMenu = false;
+
+// Set Initial State of Contact
+let showContact = false;
 
 menuBtn.addEventListener("click", toggleMenu);
 
@@ -27,9 +32,30 @@ function toggleMenu() {
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
     navItems.forEach((item) => item.classList.remove("show"));
+    contactItems.forEach((item) => item.classList.remove("display"));
 
     // Reset Menu State
     showMenu = false;
+
+    // Reset Contact State
+    showContact = false;
+  }
+}
+
+// Toggle Contact Items
+contactBtn.addEventListener("click", toggleContactItems);
+
+function toggleContactItems(e) {
+  if (!showContact) {
+    contactItems.forEach((item) => item.classList.add("display"));
+    e.preventDefault();
+    // Reset Contact State
+    showContact = true;
+  } else {
+    contactItems.forEach((item) => item.classList.remove("display"));
+    e.preventDefault();
+    // Reset Contact State
+    showContact = false;
   }
 }
 
